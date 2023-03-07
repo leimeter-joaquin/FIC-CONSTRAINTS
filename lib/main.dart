0cf0279f59adc3804cca3a1691e0c36043bb8104
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-03-07 08:37:14 -0300

    add example 7

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
                 Example4(),
                 Example5(),
                 Example6(),
+                Example7(),
               ],
             ),
           ),
@@ -68,6 +69,7 @@ class _ExamplesLayoutState extends State<ExamplesLayout> {
       children: [
         Expanded(
           child: SizedBox.expand(
+            //! This sized box serves as an example of a parent that forces the child to occupy all of the spance, horizontaly and vertically, like the root widget does. No need to go into SizedBox now.
             child: widget.examples[index - 1],
           ),
         ),
@@ -102,7 +104,8 @@ class Example1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
-      height: 40,
+      height:
+          40, //* Explain that the width is not taken into account because the Parent widget tells the child to occupy the whole space overriding these properties.
       width: 40,
     );
   }
@@ -116,9 +119,9 @@ class Example2 extends StatelessWidget {
     return DecoratedBox(
       decoration: BoxDecoration(
           color: Colors
-              .transparent), //Cambiar el bg para mostrar e lugar que ocupa el center.
+              .transparent), //** Cambiar el bg para mostrar e lugar que ocupa el center.
       child: Center(
-        // El widget Center te deja posicionar un hijo dentro de la forma que quieras. El widget Center se expande para cubrir toda el area disponible. Podríamos usar un UnconstrainedBox en su lugar.
+        //* El widget Center te deja posicionar un hijo dentro de la forma que quieras. El widget Center se expande para cubrir toda el area disponible. Podríamos usar un UnconstrainedBox en su lugar.
         child: Container(
           height: 40,
           width: 40,
@@ -135,6 +138,7 @@ class Example3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+      // TODO: add comments explaining behavior.
       child: Container(
         height: double.infinity,
         width: double.infinity,
@@ -150,12 +154,12 @@ class Example4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      // center le dice al hijo: "crecé tanto como quieras hasta w:390 y h:844"
+      //* center le dice al hijo: "crecé tanto como quieras hasta w:390 y h:844"
       child: Container(
-        // Container red dice: "okis, ya que tengo child le aviso a mi hijo 'Container blue' que sus constraints son maxWidth: 390 y maxHight: 844."
+        //* Container red dice: "okis, ya que tengo child le aviso a mi hijo 'Container blue' que sus constraints son maxWidth: 390 y maxHight: 844."
         color: Colors.red,
         child: Container(
-          // Container blue dice: "Como no tengo child no voy a tener que pasar ningun constraint hacia abajo, quisiera expanderme lo más posible, entonces como vos Container red, padre mío, me dejás expandierme hasta un maxWidth de 390 y un maxHeigth de 844 voy a hacer eso."
+          //* Container blue dice: "Como no tengo child no voy a tener que pasar ningun constraint hacia abajo, quisiera expanderme lo más posible, entonces como vos Container red, padre mío, me dejás expandierme hasta un maxWidth de 390 y un maxHeigth de 844 voy a hacer eso."
           color: Colors.blue,
         ),
       ),
@@ -169,12 +173,12 @@ class Example5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      // center le dice al hijo: "crecé tanto como quieras hasta w:390 y h:844"
+      //* center le dice al hijo: "crecé tanto como quieras hasta w:390 y h:667"
       child: Container(
-        // Container red dice: "okis, ya que tengo child le aviso a mi hijo 'Container blue' que sus constraints son maxWidth: 390 y maxHight: 844."
+        //* Container red dice: "okis, ya que tengo child le aviso a mi hijo 'Container blue' que sus constraints son maxWidth: 390 y maxHight: 844."
         color: Colors.red,
         child: Container(
-          // Container blue dice: "Como no tengo child, no tengo que pasar constraints hacia abajo PERO si tengo un height declarado, por esta razón voy a tener esa altura pero voy a expandirme lo más posible hacia los costados."
+          //* Container blue dice: "Como no tengo child, no tengo que pasar constraints hacia abajo PERO si tengo un height declarado, por esta razón voy a tener esa altura pero voy a expandirme lo más posible hacia los costados."
           color: Colors.blue,
           height: 20,
         ),
@@ -182,7 +186,7 @@ class Example5 extends StatelessWidget {
     );
   }
 }
-// Tener en cuenta que un Container renderiza un ConstrainedBox cuando tiene un height o width seteado.
+//* Tener en cuenta que un Container renderiza un ConstrainedBox cuando tiene un height o width seteado.
 
 class Example6 extends StatelessWidget {
   const Example6({super.key});
@@ -198,15 +202,42 @@ class Example6 extends StatelessWidget {
           width: 60,
           height: 60,
         ),
-        SizedBox(height: 20),
+        SizedBox(height: 20), //* Show usage of gap library.
         Expanded(
           child: Container(
             decoration: BoxDecoration(color: Colors.orange),
             width: 60,
-            height: 10, // These is ignored because of the Expanded parent.
+            height: 10, //! These is ignored because of the Expanded parent.
           ),
         ),
       ],
+    );
+  }
+}
+
+/// Overflow of text and how to solve it with Expanded widget + text style + first time showing padding.
+class Example7 extends StatelessWidget {
+  const Example7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(8),
+        color: Colors.pink[100],
+        child: Row(
+          children: [
+            //* Wrap text with expanded widget to solve this issue.
+            //! The Expanded widget works as a child of Flex, Row or Column. It lets it's (Expanded's) children to occupy the whole space in the main axis but no more.
+            Text(
+              'Lorem Overflow ipsum haha test1 test2 test3 test4 test5 test6 test7 test8 test9',
+              style: TextStyle(color: Colors.grey[800]),
+              //* maxLines: 1, //* add this after using expanded widget
+              //* overflow: TextOverflow.ellipsis, //* add this to show ellipsis
+            ), //* inspect widget.
+          ],
+        ),
+      ),
     );
   }
 }
